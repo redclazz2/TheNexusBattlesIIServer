b0aeb9b8d0412800c001ed297b2ab8ca1e20d764
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2023-08-26 05:12:32 -0500

    Init

---
+++ src/rooms/MyRoom.ts
@@ -0,0 +1,32 @@
+import { Room, Client } from "@colyseus/core";
+import { MyRoomState } from "./schema/MyRoomState";
+import { updateLobby} from "colyseus";
+
+export class MyRoom extends Room<MyRoomState> {
+  maxClients = 4;
+
+  onCreate (options: any) {
+    this.setState(new MyRoomState());
+
+    this.onMessage("type", (client, message) => {
+      //
+      // handle "type" message
+      //
+    });
+
+    updateLobby(this);
+  }
+
+  onJoin (client: Client, options: any) {
+    console.log(client.sessionId, "joined!");
+  }
+
+  onLeave (client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!");
+  }
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...");
+  }
+
+}
